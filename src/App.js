9688b1eeba05acaa59499253a939d6c5ad7c85bb
KingTemi2024 <165831988+KingTemi2024@users.noreply.github.com>
2025-05-02 19:10:44 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './App.css';
 import ReentrancyVisualization from './components/ReentrancyVisualization';
 
 function App() {
   return (
-    <div className="App min-h-screen bg-gray-100 py-8">
+    <div className="App">
       <ReentrancyVisualization />
     </div>
   );
