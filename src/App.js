9b45c9e4845e9c91c79014795244dd0130b7933c
KingTemi2024 <165831988+KingTemi2024@users.noreply.github.com>
2025-05-02 19:26:48 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ReentrancyVisualization from './components/ReentrancyVisualization';
 
 function App() {
   return (
-    <div className="App">
-      <ReentrancyVisualization />
-    </div>
+    <BrowserRouter basename="/reentrancy-visualization">
+      <Routes>
+        <Route path="/" element={<ReentrancyVisualization />} />
+        {/* Add more routes here if needed */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
