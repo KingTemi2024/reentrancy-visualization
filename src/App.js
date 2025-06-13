164bf1e2dc8bfd3484d1d82a9d35dcef12224e79
KingTemi2024 <165831988+KingTemi2024@users.noreply.github.com>
2025-06-13 00:32:38 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import './App.css';
 
-// Import your components
+// Import all your available components
 import EnhancedSmartContractPlatform from './components/EnhancedSmartContractPlatform';
 import SmartAnalyzer from './components/SmartAnalyzer';
 import SmartContractVulnerabilityPlatform from './components/SmartContractVulnerabilityPlatform';
@@ -13,23 +13,59 @@ function App() {
   const [currentComponent, setCurrentComponent] = useState('EnhancedSmartContractPlatform');
 
   const handleNavigation = (componentName) => {
+    console.log(`Navigating to: ${componentName}`);
     setCurrentComponent(componentName);
   };
 
   const renderComponent = () => {
     switch(currentComponent) {
       case 'EnhancedSmartContractPlatform':
-        return <EnhancedSmartContractPlatform onNavigationClick={handleNavigation} />;
+        return (
+          <EnhancedSmartContractPlatform 
+            onNavigationClick={handleNavigation}
+            currentPage={currentComponent}
+          />
+        );
+        
       case 'SmartAnalyzer':
-        return <SmartAnalyzer onNavigationClick={handleNavigation} />;
+        return (
+          <SmartAnalyzer 
+            onNavigationClick={handleNavigation}
+            currentPage={currentComponent}
+          />
+        );
+        
       case 'SmartContractVulnerabilityPlatform':
-        return <SmartContractVulnerabilityPlatform onNavigationClick={handleNavigation} />;
+        return (
+          <SmartContractVulnerabilityPlatform 
+            onNavigationClick={handleNavigation}
+            currentPage={currentComponent}
+          />
+        );
+        
       case 'ReentrancyVisualization':
-        return <ReentrancyVisualization onNavigationClick={handleNavigation} />;
+        return (
+          <ReentrancyVisualization 
+            onNavigationClick={handleNavigation}
+            currentPage={currentComponent}
+          />
+        );
+        
       case 'SmartContractPlatform':
-        return <SmartContractPlatform onNavigationClick={handleNavigation} />;
+        return (
+          <SmartContractPlatform 
+            onNavigationClick={handleNavigation}
+            currentPage={currentComponent}
+          />
+        );
+        
       default:
-        return <EnhancedSmartContractPlatform onNavigationClick={handleNavigation} />;
+        return (
+          <EnhancedSmartContractPlatform 
+            onNavigationClick={handleNavigation}
+            currentPage="EnhancedSmartContractPlatform"
+          />
+        );
     }
   };
 
