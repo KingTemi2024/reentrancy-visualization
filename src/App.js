7baf8aaaaa15df7ad0ee2aaf7dc71c521b1e1af1
KingTemi2024 <165831988+KingTemi2024@users.noreply.github.com>
2025-06-12 23:59:03 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 // src/App.js
-import React from 'react';
-import SmartContractPlatform from './components/SmartContractPlatform';
+import React, { useState } from 'react';
 import './App.css';
 
+// Import your components
+import EnhancedSmartContractPlatform from './components/EnhancedSmartContractPlatform';
+import SmartAnalyzer from './components/SmartAnalyzer';
+import SmartContractVulnerabilityPlatform from './components/SmartContractVulnerabilityPlatform';
+import ReentrancyVisualization from './components/ReentrancyVisualization';
+import SmartContractPlatform from './components/SmartContractPlatform';
+
 function App() {
+  const [currentComponent, setCurrentComponent] = useState('EnhancedSmartContractPlatform');
+
+  const handleNavigation = (componentName) => {
+    setCurrentComponent(componentName);
+  };
+
+  const renderComponent = () => {
+    switch(currentComponent) {
+      case 'EnhancedSmartContractPlatform':
+        return <EnhancedSmartContractPlatform onNavigationClick={handleNavigation} />;
+      case 'SmartAnalyzer':
+        return <SmartAnalyzer onNavigationClick={handleNavigation} />;
+      case 'SmartContractVulnerabilityPlatform':
+        return <SmartContractVulnerabilityPlatform onNavigationClick={handleNavigation} />;
+      case 'ReentrancyVisualization':
+        return <ReentrancyVisualization onNavigationClick={handleNavigation} />;
+      case 'SmartContractPlatform':
+        return <SmartContractPlatform onNavigationClick={handleNavigation} />;
+      default:
+        return <EnhancedSmartContractPlatform onNavigationClick={handleNavigation} />;
+    }
+  };
+
   return (
     <div className="App">
-      <SmartContractPlatform />
+      {renderComponent()}
     </div>
   );
 }
